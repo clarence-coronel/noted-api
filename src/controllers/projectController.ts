5eafac906528fc9f1d54f08e58a716598fb28841
Clarence Coronel <clarence.coronel.r@gmail.com>
2025-06-21 11:04:29 +0800

    feat(project): finalize CRUD controller and routes

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -2,13 +2,93 @@ import { Request, Response } from "express";
 import { sendError, sendSuccess } from "../utils/response";
 import { prisma } from "../utils";
 import { z } from "zod";
-import { createProjectSchema } from "../schemas";
+import { createProjectSchema, idSchema } from "../schemas";
+import { ErrorCodesEnum } from "../enums";
 
-const { project } = prisma;
+export const getAllProjects = async (request: Request, response: Response) => {
+  const ownedBy = request.user.userId;
 
-export const createProject = async (req: Request, res: Response) => {
-  const { title } = req.body as z.infer<typeof createProjectSchema>;
-  const ownedBy = req.user?.userId;
+  const projects = await prisma.project.findMany({
+    where: {
+      ownedBy,
+      deletedAt: null, //do not include deleted projects
+    },
+  });
 
-  console.log({ title, ownedBy });
+  sendSuccess({ response, data: projects });
+  return;
+};
+
+export const getProjectById = async (request: Request, response: Response) => {
+  const { id } = request.params as z.infer<typeof idSchema>;
+
+  const project = await prisma.project.findFirst({
+    where: {
+      id,
+      deletedAt: null,
+    },
+  });
+
+  if (!project) {
+    sendError({
+      response,
+      message: "Project not found",
+      code: ErrorCodesEnum.NOT_FOUND,
+      statusCode: 404,
+    });
+    return;
+  }
+
+  sendSuccess({ response, data: project });
+  return;
+};
+
+export const createProject = async (request: Request, response: Response) => {
+  const { title } = request.body as z.infer<typeof createProjectSchema>;
+  const ownedBy = request.user.userId;
+
+  const newProject = await prisma.project.create({
+    data: {
+      title,
+      ownedBy,
+    },
+  });
+
+  sendSuccess({ response, data: newProject, statusCode: 201 });
+  return;
+};
+
+export const updateProject = async (request: Request, response: Response) => {
+  const { title } = request.body as z.infer<typeof createProjectSchema>;
+  const { id } = request.params as z.infer<typeof idSchema>;
+
+  const updatedProject = await prisma.project.update({
+    where: { id },
+    data: {
+      title,
+    },
+  });
+
+  sendSuccess({
+    response,
+    data: updatedProject,
+  });
+  return;
+};
+
+export const deleteProject = async (request: Request, response: Response) => {
+  const { id } = request.params as z.infer<typeof idSchema>;
+
+  await prisma.project.update({
+    where: { id },
+    data: {
+      deletedAt: new Date(),
+    },
+  });
+
+  sendSuccess({
+    response,
+    statusCode: 204,
+  });
+  return;
 };
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
-import { createProject, deleteProject, getAllProjects } from "../controllers";
+import {
+  createProject,
+  deleteProject,
+  getAllProjects,
+  getProjectById,
+  updateProject,
+} from "../controllers";
 import { createProjectSchema, idSchema } from "../schemas";
 import { validateBody, validateParams } from "../middleware";
 import { auth } from "../middleware/auth";
@@ -9,7 +15,14 @@ const router = Router();
 router.use(auth);
 
 router.get("/", getAllProjects);
+router.get("/:id", validateParams(idSchema), getProjectById);
 router.post("/", validateBody(createProjectSchema), createProject);
+router.put(
+  "/:id",
+  validateParams(idSchema),
+  validateBody(createProjectSchema),
+  updateProject
+);
 router.delete("/:id", validateParams(idSchema), deleteProject);
 
 export default router;
